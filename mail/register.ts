13b00a1dbaa3006dd8ad55d9bb5c59b4e55dec1f
Xanday <xandaychannel@gmail.com>
2021-08-06 23:29:47 +0300

    06/08/2021

--- mail/register.ts
+++ mail/register.ts
@@ -5,7 +5,7 @@ export async function SendMessage(email:string, pincode:string) {
     const message = new Message({
         from: 'tiafy_official@outlook.com',
         to: `${email}`,
-        subject: 'Tiafy - Регистрация аккаунда',
+        subject: 'Tiafy - Регистрация аккаунта',
         text: `Ссылка для подтверждения регистрации: http://localhost:3000/confirm-registration?pincode=${pincode}. Не давайте её никому`
     });
 
